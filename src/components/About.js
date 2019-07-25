164061b724ce5e0ef0f2f4165808883c2d960b1a
mmboyce <mathieumimmsboyce@gmail.com>
2019-07-24 21:31:28 -0400

    added icon

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,14 @@ import { fab } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 library.add(fab, faEnvelope);
+const me = require('../images/me.jpg');
+
 
 class About extends React.Component{
     render(){
         return (
             <div id="about" class="section">
+                <img id="me" src={me} alt=""/>
                 <h1><span id="first">W Mathieu</span> Mimms-Boyce</h1>
                 <a href="mailto:mathieumimmsboyce@gmail.com">
                     <FontAwesomeIcon id="email" icon="envelope"/>
