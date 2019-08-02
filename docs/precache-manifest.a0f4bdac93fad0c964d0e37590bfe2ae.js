9fed97dbeaa59495023ec3109b44baa125609ca9
mmboyce <mathieumimmsboyce@gmail.com>
2019-08-02 15:35:45 -0400

    build to docs

---
+++ docs/precache-manifest.a0f4bdac93fad0c964d0e37590bfe2ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1211fec0b8fd17ce087a0d2b9a6802c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5a0c4facff72e7e5709",
+    "url": "/static/css/main.77511aae.chunk.css"
+  },
+  {
+    "revision": "9e43a62684c5492d05b3",
+    "url": "/static/js/2.5c114628.chunk.js"
+  },
+  {
+    "revision": "b5a0c4facff72e7e5709",
+    "url": "/static/js/main.b3b0bf70.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "04511473cbfa0e2f11b4e896e008e9a9",
+    "url": "/static/media/me.04511473.jpg"
+  },
+  {
+    "revision": "aea61dbafddbf335f95852a07330a173",
+    "url": "/static/media/me2.aea61dba.jpg"
+  }
+]);
