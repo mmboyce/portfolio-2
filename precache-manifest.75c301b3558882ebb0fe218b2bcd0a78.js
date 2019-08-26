68ab2eed283f7e26089af9707a90d85d0fbc3b28
mmboyce <mathieumimmsboyce@gmail.com>
2019-08-26 15:49:27 -0400

    quick fix: attempt 2

---
+++ precache-manifest.75c301b3558882ebb0fe218b2bcd0a78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e8995a1db0e7fb698fad49a90c5e3dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10f886d5e445edfc668e",
+    "url": "/static/css/main.6b7b8661.chunk.css"
+  },
+  {
+    "revision": "31520abac89b61f0c572",
+    "url": "/static/js/2.f6afbe1c.chunk.js"
+  },
+  {
+    "revision": "10f886d5e445edfc668e",
+    "url": "/static/js/main.0db5ddd1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "04511473cbfa0e2f11b4e896e008e9a9",
+    "url": "/static/media/me.04511473.jpg"
+  },
+  {
+    "revision": "aea61dbafddbf335f95852a07330a173",
+    "url": "/static/media/me2.aea61dba.jpg"
+  }
+]);
